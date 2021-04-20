ce5c1b44181d4b3cd17aea4d733288f9eb779708
Ederson Peka <ederson@gmail.com>
2021-04-20 12:36:50 -0300

    Fix date format and implode parameter order (#5)
    - Use correct time zone (GMT) for date headers
    - Fixed bug: parameter order for implode (PHP 8 compatibility)
    
    * Fixed bug: server expects Greenwich Mean Time (GMT)

--- SerialsSolutions/Summon/Base.php
+++ SerialsSolutions/Summon/Base.php
@@ -246,10 +246,10 @@ abstract class SerialsSolutions_Summon_Base
         // Build Authorization Headers
         $headers = array(
             'Accept' => 'application/'.$this->responseType,
-            'x-summon-date' => date('D, d M Y H:i:s T'),
+            'x-summon-date' => gmdate('D, d M Y H:i:s T'),
             'Host' => 'api.summon.serialssolutions.com'
         );
-        $data = implode($headers, "\n") . "\n/$this->version/$service\n" .
+        $data = implode("\n", $headers) . "\n/$this->version/$service\n" .
             urldecode($queryString) . "\n";
         $hmacHash = $this->hmacsha1($this->apiKey, $data);
         $headers['Authorization'] = "Summon $this->apiId;$hmacHash";
